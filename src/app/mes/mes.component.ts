c5ad849af5b10c805e393c1c508995f019de79a3
Gabri <gabriel.avila-munoz@capgemini.com>
2020-10-06 20:39:15 +0200

    Calendario incompleto pero funciona

--- src/app/mes/mes.component.ts
+++ src/app/mes/mes.component.ts
@@ -1,3 +1,4 @@
+import { newArray } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { Dia, Estado } from '../clases/app.persona';
 
@@ -15,7 +16,7 @@ export class MesComponent implements OnInit {
   diasMiercoles:Array<Dia>
 
   datosDia:Array<Dia>
-  todosDias:Array<Array<Dia>>
+  matrizMes:Array<Array<Dia>>
 
   //dia:Dia
 
@@ -27,15 +28,15 @@ export class MesComponent implements OnInit {
     this.diasMartes = new Array;
     this.diasMiercoles = new Array;
 
-    this.todosDias = new Array;
+    this.matrizMes = new Array;
 
     this.datosDia = new Array;
-    this.datosDia.push(new Dia(7,1,4,new Estado(5,"LABORABLE",1)));
-    this.datosDia.push(new Dia(8,2,4,new Estado(6,"FESTIVO",1)))
-    this.datosDia.push(new Dia(9,3,4,new Estado(5,"LABORABLE",1)));
-    this.datosDia.push(new Dia(10,1,4,new Estado(6,"FESTIVO",1)))
-    this.datosDia.push(new Dia(11,2,4,new Estado(5,"LABORABLE",1)));
-    this.datosDia.push(new Dia(12,3,4,new Estado(6,"FESTIVO",1)))
+    this.datosDia.push(new Dia(7,0,4,new Estado(5,"LABORABLE",1)));
+    this.datosDia.push(new Dia(8,1,5,new Estado(6,"FESTIVO",1)))
+    this.datosDia.push(new Dia(9,2,5,new Estado(5,"LABORABLE",1)));
+    this.datosDia.push(new Dia(10,0,5,new Estado(6,"FESTIVO",1)))
+    this.datosDia.push(new Dia(11,1,6,new Estado(5,"LABORABLE",1)));
+    this.datosDia.push(new Dia(12,2,6,new Estado(6,"FESTIVO",1)))
    }
 
   ngOnInit(): void {
@@ -43,7 +44,38 @@ export class MesComponent implements OnInit {
   }
 
   separarCalendarioDiasSemana(){
+    let semana:Array<Dia>
+    let semanaMesRef = this.datosDia[0].semanaMes;
+    semana = new Array;
+    let relleno = 0;
+
+    //Relleno antes del primer dia con dias = 0
+      //Determinamos los dias a rellenar
+    if(this.datosDia[0].diaSemana==0){
+      relleno = this.diasSemana.length -1;
+    }else if(this.datosDia[0].diaSemana > 1){
+      relleno = this.datosDia[0].diaSemana-1;
+    }
+      //Rellenamos los dias vacios
+    for(let i=0; i<relleno; i++){
+      semana.push(new Dia(0,null,null,null));
+    }
+
+    //Relleno con el resto con los datos
     this.datosDia.forEach(dia=>{
+      if(dia.semanaMes == semanaMesRef){
+        semana.push(dia);
+      }else{
+        this.matrizMes.push(semana);
+        semanaMesRef = dia.semanaMes;
+        semana = []
+        semana.push(dia);
+      }
+    })
+    this.matrizMes.push(semana);
+    console.log(this.matrizMes)
+
+    /*this.datosDia.forEach(dia=>{
       switch(dia.diaSemana){
         case 1:
           this.diasLunes.push(dia);
@@ -61,6 +93,6 @@ export class MesComponent implements OnInit {
     this.todosDias.push(this.diasLunes);
     this.todosDias.push(this.diasMartes);
     this.todosDias.push(this.diasMiercoles);
-    console.log(this.todosDias)
+    console.log(this.todosDias)*/
   }
 }
