e5d5068b0e040dede35242207354a49d201e2a9a
Carlos Tarancón <airedits97@gmail.com>
2020-10-08 13:02:58 +0200

    menu + jornadas + empleados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
+import { JornadasComponent } from './jornadas/jornadas.component';
 
 
 const routes: Routes = [
   {
     path:'empleado',
     component: EmpleadosComponent
+  },
+  {
+    path: 'jornadas',
+    component: JornadasComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JornadasComponent } from './jornadas/jornadas.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { EmpleadoFormularioComponent } from './empleado-formulario/empleado-formulario.component';
+import { JornadaFormComponent } from './jornada-form/jornada-form.component';
+import { JornadaTableComponent } from './jornada-table/jornada-table.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -14,11 +19,18 @@ import { EmpleadoFormularioComponent } from './empleado-formulario/empleado-form
     JornadasComponent,
     EmpleadosComponent,
     EmpleadoFormularioComponent,
+    JornadaFormComponent,
+    JornadaTableComponent,
+    JornadasComponent,
+    NavBarComponent
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CommonModule,
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
